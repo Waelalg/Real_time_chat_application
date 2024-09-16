35e5982b7d07eaaf3ab90d1becfbe304cd4802e9
Waelalg <mo.lebaili@esi-sba.dz>
2024-09-16 18:07:02 +0100

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -12,12 +13,21 @@ dotenv.config();
 
 
 const PORT = process.env.PORT || 4000;
+
+const __dirname = path.resolve();
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth" , authRoutes);
 app.use("/api/messages" , messageRoutes);
 app.use("/api/users" , usersRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT,()=>{
     connectDB();
