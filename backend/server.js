1d137a3f7800ce6e07bd16b0881f1a457c797d6c
Waelalg <mo.lebaili@esi-sba.dz>
2024-09-16 17:41:23 +0100

    work is done

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from "cors"
 import authRoutes from '../backend/routes/authRoutes.js';
 import messageRoutes from '../backend/routes/messageRoutes.js';
 import usersRoutes from '../backend/routes/usersRoutes.js';
 import connectDB from './db/connectToMongoDb.js';
+import { app,server } from './socket/socket.js';
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 4000;
 
+const PORT = process.env.PORT || 4000;
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth" , authRoutes);
 app.use("/api/messages" , messageRoutes);
 app.use("/api/users" , usersRoutes);
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     connectDB();
     console.log(`server is running on ${PORT}`)
 });
--- frontend/src/components/Sidebar/Conversation.jsx
+++ frontend/src/components/Sidebar/Conversation.jsx
@@ -1,9 +1,12 @@
+import { useSocketContext } from "../../context/SocketContext";
 import useConversation from "../../zustand/useConversations";
 
-const Conversation = ({ conversation, lastIdx}) => {
+const Conversation = ({ conversation, lastIdx, emoji }) => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
 
 	const isSelected = selectedConversation?._id === conversation._id;
+	const { onlineUsers } = useSocketContext();
+	const isOnline = onlineUsers.includes(conversation._id);
 
 	return (
 		<>
@@ -13,7 +16,7 @@ const Conversation = ({ conversation, lastIdx}) => {
 			`}
 				onClick={() => setSelectedConversation(conversation)}
 			>
-				<div className={`avatar online`}>
+				<div className={`avatar ${isOnline ? "online" : ""}`}>
 					<div className='w-12 rounded-full'>
 						<img src={conversation.profilePic} alt='user avatar' />
 					</div>
@@ -22,7 +25,7 @@ const Conversation = ({ conversation, lastIdx}) => {
 				<div className='flex flex-col flex-1'>
 					<div className='flex gap-3 justify-between'>
 						<p className='font-bold text-gray-200'>{conversation.fullName}</p>
-						<span className='text-xl'></span>
+						<span className='text-xl'>{emoji}</span>
 					</div>
 				</div>
 			</div>
@@ -32,3 +35,4 @@ const Conversation = ({ conversation, lastIdx}) => {
 	);
 };
 export default Conversation;
+
